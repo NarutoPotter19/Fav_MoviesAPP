d8615d3b889d6342ddd6ca9284a2862999e51109
shivankit singh <shivankitsingh19@gmail.com>
2022-07-15 17:59:51 +0530

    setting up frontend and component of webpage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { data } from './data.js';
+import Navbar from './Navbar.js';
+import MovieCard from './MovieCard.js'
 
 
 function App() {
   return (
-    <h1>project start</h1>
+    <div className='App'>
+      <Navbar />
+      <div className='main'></div>
+      <div className='tabs'>
+        <div className='tab'></div>
+        <div className='tab'></div>
+      </div>
+
+      <div className='list'>
+        {data.map(movie =>(
+          <MovieCard movie={movie}/>
+        ))}
+      </div>
+    </div>
+    
   );
 }
 
