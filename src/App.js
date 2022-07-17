3914c1e0983bfabd3d82d75f907887ed971b03c3
shivankit singh <shivankitsingh19@gmail.com>
2022-07-17 13:23:35 +0530

    subscribing to the store

--- src/App.js
+++ src/App.js
@@ -4,24 +4,43 @@ import Navbar from './Navbar.js';
 import MovieCard from './MovieCard.js'
 
 
-function App() {
+class App extends React.Component{
+  componentDidMount () {
+
+    const {store} =  this.props;
+    store.subscribe(()=>{
+      console.log ('UPDATED');
+      this.forceUpdate();
+    });
+    // to make a
+    store.dispatch({
+      type: 'ADD_MOVIES',
+      movies : data
+    });
+   
+    console.log('STATE',this.props.store.getState());
+  }
+  render() {
+    const movies = this.props.store.getState();
+    console.log('RENDER');
   return (
     <div className='App'>
       <Navbar />
       <div className='main'></div>
       <div className='tabs'>
-        <div className='tab'></div>
-        <div className='tab'></div>
+        <div className='tab'>Movies</div>
+        <div className='tab'>Favourite</div>
       </div>
 
       <div className='list'>
-        {data.map(movie =>(
-          <MovieCard movie={movie}/>
+        {data.map((movie,index) =>(
+          <MovieCard movie={movie} key={'movies-${index}'}/>
         ))}
       </div>
     </div>
     
   );
+}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,13 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import movies from './ruducers/rindex';
+
+const store = createStore(movies);
+console.log('store' , store);
+/*
+console.log('BEFORE STATE' , store.getState());
+
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{ name : 'Man of Steel'}]
+});
+console.log('AFTER STATE' , store.getState());*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
+    <App  store={store}/>
   </React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
